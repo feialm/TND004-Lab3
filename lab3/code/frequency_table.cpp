d28b50b3cf28fc8fcf820c7f26b09e60b33c46f5
ElinDjurberg <57451642+ElinDjurberg@users.noreply.github.com>
2021-05-01 16:55:11 +0200

    Stuck on operator<

--- lab3/code/frequency_table.cpp
+++ lab3/code/frequency_table.cpp
@@ -5,6 +5,12 @@
 #include <algorithm>
 #include <cassert>  //assert
 
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <ostream>
+
+
 #include "BinarySearchTree.h"
 
 #define TEST_EXERCISE3
@@ -16,73 +22,136 @@
 #ifdef TEST_EXERCISE3
 
 
-struct Row {
-    // ADD CODE: exercise 3
+struct Row
+{
+	// ADD CODE: exercise 3
 
 	std::string key = "";
 	int counter = 0;
-	Row* left = nullptr;
-	Row* right = nullptr;
-
-
-	 // Constructors
-	Row(std::string& theWord, int theCount, Row* lt = nullptr, Row* rt = nullptr)
-		: key{ theWord }, counter{ theCount }, left{ lt }, right{ rt }  {
-		//heheheh
-	}
+	
 	/*
-
-	 //Copy constructors -- disallowed
-    Row(const Row&) = delete;
-
-    //Assignement operator -- disallowed
-    Row& operator=(const Row&) = delete;
-
-    // Destructor
-    ~Row() {
-        --count_nodes;
-        assert(count_nodes >= 0);  // number of existing nodes can never be negative
-    }
-
-    static int count_nodes;  // total number of existing nodes -- to help to detect bugs in the code
+	// Constructors
+	Row(std::string word, int number)
+		: key{ word }, counter{ number }{
+		//++count_nodes;
+	}
 	*/
-
-private:
-
+	/*
+	bool operator<(const Row& rhs)const {
+		if (key < rhs.key) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	*/
 };
 
-
 // Add other stuff, if needed
-Row* insert(std::string key)
+
+
+std::ostream& operator<<(std::ostream& os, const Row& dt)
 {
+	os << dt.key; //något liknande men detta är fel, men man ska nå key
+	return os;
 
-	// set count as 1 and left
-	// and right children as NULL
-	Row* newnode = new Row{"", 0, nullptr,nullptr}; //KOLLA VARFÖR DETTA EJ FUNKAR
-	newnode->key = key;
-	newnode->counter = 1;
-	newnode->left = newnode->right = NULL;
-
-	return newnode;
 }
+
+
+
 /*
-Row end() //const
+//Vi returnerar den minsta
+bool operator<(const Row& a, const Row& b)
 {
 
-	return Row(nullptr, nullptr);
-}
-
-Row begin()
-{
-	if (isEmpty())return end();
-	return Row(findMin(root), this);
-}
-//Vad ska denna göra??
-Row& operator=(BinarySearchTree _copy) {
-		std::swap(root, _copy.root);
-		return *this;
+	if(a.key < b.key)
+	{
+		return true;
+	}else{
+		return false;
 	}
+	
+}
 */
+/*
+Row& operator<(const Row& a, const Row& b)
+{
+
+	if(a.key < b.key)
+	{
+		return a;
+	}else{
+		return b;
+	}
+
+}
+
+//Vi returnerar den minsta
+bool operator<(const Compareble& a, const Row& b)
+{
+
+	if(a.key < b.key)
+	{
+		return true;
+	}else{
+		return false;
+	}
+
+}
+
+
+//Vi returnerar den minsta
+bool operator<(const Row& a, const Compareble& b)
+{
+
+	if(a.key < b.key)
+	{
+		return true;
+	}else{
+		return false;
+	}
+
+}
+*/
+
+
+/*
+Row* insert( std::string x, Row* t) {
+
+	if (t == nullptr) // insert after a leaf
+	{
+		std::string s = "";
+		// förälder är nullptr i nuläget eftersom man inte vet vem föräldern är än
+		t = new Row{ s, 0, nullptr, nullptr };
+
+	}
+	else if (x < t->key)		//(x < t->key)
+	{
+		Row* newRow = insert(x, t->left); // får en ny nod från if-satsen ovanför
+
+		t->left = newRow; //t->left = förälder till NewNode
+
+		newRow->parent = t; // tillger en förälder t till den nya noden newNode
+
+	}
+	else if (t->key < x)
+	{
+		Row* newRow = insert(x,t->right); // får en ny nod från översta if-satsen 
+
+		t->right = newRow;  //t->right = förälder till NewNode
+
+		newRow->parent = t; // tillger en förälder t till den nya noden newNode
+	}
+	else
+	{
+		;  // Duplicate; do nothing
+	}
+	return t;
+}
+*/
+
+
 
 #endif
 
@@ -101,15 +170,19 @@ void exercise3() {
             return;
         }
 
-        // ADD CODE: exercise 3
+		// ADD CODE: exercise 3
+		
+		//SKA VI HA PAIR HÄR?
+		std::vector<std::string> V;
 
-        while(file >> word){
+
+        while(file >> word)
+		{
         
          // för att to-low orden
 		//Den 3 word.begin() säger att vi vill lägga in/override transformationen i början av ordet. Så Elin blir elin
 		//Om vi hade haft word.end() där så hade det blivit ex Elinelin för vi börjar inserta i slutet av ordet
-		//std::transform(word.begin(), word.end(), word.begin(), [](char c) {return static_cast<char>(std::tolower(c)); });
-
+		std::transform(word.begin(), word.end(), word.begin(), [](char c) {return static_cast<char>(std::tolower(c)); });
 
 
 		// std::remove(word.begin(), word.end(), ',' är en iterator med villkor, vad den ska göra
@@ -117,7 +190,7 @@ void exercise3() {
 		// remove skapar en iteror som letar upp alla komma-tecken
 		// erase använder iteratorn för att ta bort ','
 		// word.erase, det är funktionen erase som tar bort tecknen från objektet word
-		/*
+		
 		word.erase(std::remove(word.begin(), word.end(), ','), word.end());
 		word.erase(std::remove(word.begin(), word.end(), '.'), word.end());
 		word.erase(std::remove(word.begin(), word.end(), '"'), word.end());
@@ -128,16 +201,41 @@ void exercise3() {
 		word.erase(std::remove(word.begin(), word.end(), '/'), word.end());
 		word.erase(std::remove(word.begin(), word.end(), ';'), word.end());
 		word.erase(std::remove(word.begin(), word.end(), ':'), word.end());
-		*/
+		
 		//remove_if kan man göra det i en linje
+		V.push_back(word);
+		
+		// Read all words in the file into a vector
+		//std::vector<std::string> V{ std::istream_iterator<std::string>{word},
+								  // std::istream_iterator<std::string>{} };
 
 
-        
+		file.close();
+
+
+
+		std::cout << "\nMade Innan loop\n";
+
+
+		// Create a tree
+		BinarySearchTree<Row> t; //Binärt träd med rader.
+
+		Row r;
+		r.key = V[2];
+		t.insert(r); //sätter in ett objekt från row r i tree t
+	
+		std::cout << "\nSTART TREE\n";
+		// Display the tree
+		std::cout << "Tree: \n";
+		t.printTree();
+		std::cout << '\n';
+		std::cout << "\nEND TREE\n";
         }
 
 
 
     }
+	
 	assert(BinarySearchTree<Row>::get_count_nodes() == 0);
 
     std::cout << "Success!!\n";
